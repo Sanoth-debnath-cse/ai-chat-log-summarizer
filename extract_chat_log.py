dcbf015a0dcc7e556b3017f3c80a36e261ba61bc
Sanoth-Repliq <sanoth.debnath@repliq.dev>
2025-05-20 00:37:55 +0600

    Filtered top 5 frequent words

--- extract_chat_log.py
+++ extract_chat_log.py
@@ -4,9 +4,9 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 # download nltk packeges
-nltk.download('punkt_tab')
-nltk.download("punkt")
-nltk.download("stopwords")
+#nltk.download('punkt_tab')
+#nltk.download("punkt")
+#nltk.download("stopwords")
 
 
 
@@ -35,12 +35,27 @@ for user,message in pattern:
 user_messages_count=len(dialogue["User"])
 ai_messages_count=len(dialogue["AI"])
 
-# Clean sample content
-clean_content=re.sub(r'[^A-Za-z0-9\s]', '', content)
+# Clean sample content by removing speaker
+clean_content = re.sub(r'(User|AI):\s*', '', content)
+
+# Clean sample content by removing special char
+clean_content=re.sub(r'[^A-Za-z0-9\s]', '', clean_content)
 
 # Tokenize and store English stop words
-filtered_content=word_tokenize(clean_content)
+filtered_content=word_tokenize(clean_content.lower())
 stop_word = set(stopwords.words("english"))
 
 # Exclude common stop words
 filtered_word=[word for word in filtered_content if word not in stop_word]
+
+# Frequency count of filtered words
+word_frequency={}
+
+for word in filtered_word:
+    word_frequency[word]=word_frequency.get(word,0)+1
+    
+print(word_frequency)
+
+# Top 5 common frequently used words
+most_frequent_5_words=sorted(word_frequency,key=lambda word:word_frequency[word],reverse=True)[:5]
+print(most_frequent_5_words)
